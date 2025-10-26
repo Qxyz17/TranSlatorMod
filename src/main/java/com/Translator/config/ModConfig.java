0c9617e551935f3ea489690543f7c93a3c21fc8b
Qxyz17 <18859251107@163.com>
2025-10-26 10:57:58 +0800

    Update ModConfig.java

--- src/main/java/com/Translator/config/ModConfig.java
+++ src/main/java/com/Translator/config/ModConfig.java
@@ -1,48 +1,7 @@
-package com.translator.config;
+package com.Translator.config;
 
-import net.minecraftforge.common.config.Configuration;
-import java.io.File;
-
+// 翻译开关配置（控制是否开启翻译）
 public class ModConfig {
-    // 翻译开关（默认开启）
-    public static boolean translateOthers = true;  // 他人消息（英译中）
-    public static boolean translateSelf = true;    // 自己消息（中译英）
-
-    private static Configuration config;
-
-    public static void init(File configFile) {
-        config = new Configuration(configFile);
-        syncConfig();
-    }
-
-    private static void syncConfig() {
-        config.load();
-        // 读取配置
-        translateOthers = config.getBoolean(
-            "translateOthers", 
-            "general", 
-            true, 
-            "Translate others' English messages to Chinese"
-        );
-        translateSelf = config.getBoolean(
-            "translateSelf", 
-            "general", 
-            true, 
-            "Translate your Chinese messages to English"
-        );
-        // 保存配置
-        if (config.hasChanged()) {
-            config.save();
-        }
-    }
-
-    // 动态更新配置（命令控制时调用）
-    public static void updateConfig(boolean isOthers, boolean state) {
-        if (isOthers) {
-            translateOthers = state;
-        } else {
-            translateSelf = state;
-        }
-        syncConfig();
-    }
+    public static boolean translateOthers = true; // 他人消息翻译（默认开启）
+    public static boolean translateSelf = true;   // 自己消息翻译（默认开启）
 }
